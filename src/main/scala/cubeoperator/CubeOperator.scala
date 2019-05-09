88fce8bd9f58fe7476acf1c98a2bfd7c6e02d39f
Marshall Cooper <mrc38@uw.edu>
2019-05-09 09:10:23 -0700

    Started implementation of cube_naive

--- src/main/scala/cubeoperator/CubeOperator.scala
+++ src/main/scala/cubeoperator/CubeOperator.scala
@@ -29,8 +29,32 @@ class CubeOperator(reducers: Int) {
 
   def cube_naive(dataset: Dataset, groupingAttributes: List[String], aggAttribute: String, agg: String): RDD[(String, Double)] = {
 
-    //TODO naive algorithm for cube computation
-    null
+    val rdd = dataset.getRDD()
+    val schema = dataset.getSchema()
+
+    val index = groupingAttributes.map(x => schema.indexOf(x))
+    val indexAgg = schema.indexOf(aggAttribute)
+
+    // Create lattice space as List[List[Int]] - each List[Int] is a configuration of inputs
+    val lattice = index.toSet.subsets().map(x=>x.toList).toList
+
+    // Map function - transform tuple t into (k,t) pair, where k is the grouping key
+    // Function: Row => List[(List[Any], Row)]
+    val mapPhase = {
+      (t: org.apache.spark.sql.Row) => lattice.map(x => (x.map(i => t(i)), t))
+    }
+    // Reduce function - transform a (k, [values]) pair into aggregation
+    // Function: (k, Row) => (String, Double)
+    // TODO: Extend this for other aggregation operations and column types
+    // TODO: Modify output String with * for 'all columns'
+    val reducePhase = {
+      (t: (List[Any], scala.Iterable[(List[Any], org.apache.spark.sql.Row)])) =>
+      (t._1.mkString(","),t._2.map(x=>x._2(indexAgg).asInstanceOf[Int].toDouble).sum)
+    }
+
+    rdd.flatMap(mapPhase)
+      .groupBy(k=>k._1)
+      .map(reducePhase)
   }
 
 }
