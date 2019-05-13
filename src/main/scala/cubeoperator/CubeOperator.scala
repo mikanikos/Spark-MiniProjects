3988977f6377d73cf8aec55952ed5a57b1ea554e
Marshall Cooper <mrc38@uw.edu>
2019-05-13 04:20:23 -0700

    Implemented cubeoperator

--- src/main/scala/cubeoperator/CubeOperator.scala
+++ src/main/scala/cubeoperator/CubeOperator.scala
@@ -2,8 +2,6 @@ package cubeoperator
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.catalyst.parser.SqlBaseParser.SetQuantifierContext
-import org.dmg.pmml.True
 
 class CubeOperator(reducers: Int) {
 
@@ -25,9 +23,67 @@ class CubeOperator(reducers: Int) {
     val index = groupingAttributes.map(x => schema.indexOf(x))
     val indexAgg = schema.indexOf(aggAttribute)
 
-    //TODO Task 1
+    val typeName = rdd.first().schema.fields(indexAgg).dataType.typeName
 
-    null
+    // Casts numeric data types into their respective type for aggregation
+    val cast = (x: Any) => typeName match {
+      case "integer" => x.asInstanceOf[Int] // Infers further cast Int->Double
+      case "double" => x.asInstanceOf[Double]
+      case _ => Double.NaN
+    }
+
+    val aggf = (ds: (Double, Double)) =>
+    {
+      agg match {
+        case "COUNT" => ds._2
+        case "AVG" => ds._1/ds._2
+        case _ =>  ds._1
+      }
+    }
+
+    // Helper function to construct the name of the result
+    val makeName = {
+      (ks: Map[Int,Any]) => index.map(i=>if(ks.contains(i)) ks(i).toString else '*').mkString(",")
+    }
+
+    // Create lattice space C mentioned in literature
+    val lattice = index.toSet.subsets().map(x=>x.toList).toList
+
+    val getKey = (r: Row) => index.map(i => (i, r(i))).toMap
+
+    // Maps Iterator[Row] => Iterator[(k, v)] where k is grouping values and v is (sum, count)
+    val mapPhase = (x: Iterator[Row]) => x.map(r => (getKey(r), (cast(r(indexAgg)), 1.0)))
+    // Combines within partition: Iterator[(k, v)] => Iterator[(k, v)] with like keys combined
+    val combine = (t: Iterator[(Map[Int, Any], (Double ,Double))]) => {
+      val m = scala.collection.mutable.HashMap[Map[Int,Any], (Double, Double)]().withDefaultValue((0.0,0.0))
+      t.foreach(r => {
+        val ds = m(r._1)
+        val rval = r._2
+        m(r._1) = (ds._1+rval._1,ds._2+rval._2)
+      })
+      m.iterator
+    }
+    // Encapsulate map and mapper-side combine into single mapper function
+    val mapCombine = (x: Iterator[Row]) => combine(mapPhase(x))
+
+    // Reduce function - transforms a (k, [values]) pair into (k, M[values]) by pairwise operation on values
+    val reducePhase = {
+      (ds1 : (Double, Double), ds2: (Double, Double)) => (ds1._1+ds2._1, ds1._2+ds2._2)
+    }
+
+    // Expands the from a bottom lattice (k, v) pair the partial results above
+    val expandLattice = (t: (Map[Int,Any], (Double, Double))) => {
+      lattice.map(lx => {
+        (t._1.filterKeys(lx.contains).toArray.toMap, t._2)
+      })
+    }
+
+    rdd.mapPartitions(mapCombine) // Map phase
+      .reduceByKey(reducePhase) // Reduce phase
+      .flatMap(expandLattice) // Expand lattice
+      .mapPartitions(combine) // Expanded map/combine
+      .reduceByKey(reducePhase) // Expanded reduction
+      .map(x => (makeName(x._1), aggf(x._2)))
   }
 
   def cube_naive(dataset: Dataset, groupingAttributes: List[String], aggAttribute: String, agg: String): RDD[(String, Double)] = {
@@ -50,16 +106,12 @@ class CubeOperator(reducers: Int) {
     }
 
     // Define measure function M referenced in literature
-    // TODO: Compute average efficiently (one pass)
-    val aggf = (ls: Iterable[Any]) =>
+    val aggf = (ds: (Double, Double)) =>
     {
       agg match {
-        case "COUNT" => ls.size.asInstanceOf[Double]
-        case "SUM" => ls.map(cast).sum
-        case "MIN" => ls.map(cast).min
-        case "MAX" => ls.map(cast).max
-        case "AVG" => ls.map(cast).sum/ls.size.asInstanceOf[Double]
-        case _ => throw new IllegalArgumentException(agg + " is not a valid aggregation function")
+        case "COUNT" => ds._2
+        case "AVG" => ds._1/ds._2
+        case _ =>  ds._1
       }
     }
 
@@ -70,18 +122,21 @@ class CubeOperator(reducers: Int) {
 
     // Map function - transform tuple t into (k,t(i)) pairs, where k is key, t(i) is aggregation column
     val mapPhase = {
-      (t: Row) => lattice.map(x => (x.map(i => (i,t(i))).toMap, t(indexAgg)))
+      (t: Row) => lattice.map(x => (x.map(i => (i,t(i))).toMap, ((cast(t(indexAgg))), 1.0)))
     }
 
-    // Reduce function - transform a (k, [values]) pair into (k, M([values]))
-    val reducePhase = {
-      (t: (Map[Int, Any], Iterable[Any])) =>
-      (makeName(t._1),aggf(t._2))
+    // Reduce function - transforms a (k, [values]) pair into (k, M([values])) by pairwise operation on values
+    val reducePhase = (d1: (Double, Double), d2: (Double, Double)) => {
+      agg match {
+        case "MIN" => (Math.min(d1._1, d2._1), 0.0)
+        case "MAX" => (Math.max(d1._1, d2._1), 0.0)
+        case _ => (d1._1+d2._1, d1._2+d2._2)
+      }
     }
 
-    // Map -> Sort/Shuffle -> Reduce
+    // Map -> Sort/Shuffle -> Reduce -> Format Output
     rdd.flatMap(mapPhase)
-      .groupByKey()
-      .map(reducePhase)
+      .reduceByKey(reducePhase)
+      .map(x => (makeName(x._1), aggf(x._2)))
   }
 }
--- src/main/scala/cubeoperator/Main.scala
+++ src/main/scala/cubeoperator/Main.scala
@@ -34,7 +34,9 @@ object Main {
 
     var groupingList = List("lo_suppkey","lo_shipmode","lo_orderdate")
 
-    val res = cb.cube(dataset, groupingList, "lo_supplycost", "SUM")
+    val res = cb.cube(dataset, groupingList, "lo_supplycost", "AVG")
+
+    res.sortBy(x=>x._1)foreach(x=>println(x))
 
     /*
        The above call corresponds to the query:
@@ -45,10 +47,8 @@ object Main {
 
 
     //Perform the same query using SparkSQL
-    //    val q1 = df.cube("lo_suppkey","lo_shipmode","lo_orderdate")
-    //      .agg(sum("lo_supplycost") as "sum supplycost")
-    //    q1.show
-
-
+        val q1 = df.cube("lo_suppkey","lo_shipmode","lo_orderdate")
+          .agg(avg("lo_supplycost") as "sum supplycost")
+      .foreach(x=>println(x))
   }
 }
